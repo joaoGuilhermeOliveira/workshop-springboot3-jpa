0209e53e2d7ab3e2087d2e3043b9e53cdc1063d0
joaoGuilhermeOliveira <joaogoliveira14@gmail.com>
2024-04-19 10:14:19 -0300

    Payment, one-to-one association

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -29,6 +29,9 @@ public class Order implements Serializable {
     @OneToMany(mappedBy = "id.order")
     private Set<OrderItem> items = new HashSet<>();
 
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL) //Mapear relação 1 para 1 com o mesmo id -> Pagamento vai ter o mesmo id do pedido em que está relacionado
+    private Payment payment;
+
     public Order(){
 
     }
@@ -74,6 +77,13 @@ public class Order implements Serializable {
         this.client = client;
     }
 
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
+    }
 
     public Set<OrderItem> getItems() {
         return items;
